e94931748751a90d08ac1b341b3a6f6fd7b1ad42
Muneeb Rana <muneebrana497@gmail.com>
2026-06-11 13:48:34 +0500

    first commit

---
+++ server/utils/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+
+const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development", 
+    sameSite: "none", 
+    maxAge: 1 * 24 * 60 * 60 * 1000, 
+  });
+};
+
+export default createJWT;
